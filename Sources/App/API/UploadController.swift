50eba0c0d3a5649fe2a8b0b335269a101d04d9d4
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-13 20:36:24 +0100

    Update upload controller to upload and return the image

--- Sources/App/API/UploadController.swift
+++ Sources/App/API/UploadController.swift
@@ -1,8 +1,24 @@
 import HTTP
+import CoreService
 
 struct UploadController {
   
+  private let uploadImage: UploadImage
+  private let imageViewMapper: ImageViewMapper
+  
+  init(uploadImage: UploadImage,
+       imageViewMapper: ImageViewMapper)
+  {
+    self.uploadImage = uploadImage
+    self.imageViewMapper = imageViewMapper
+  }
+  
   func upload(with request: Request) throws -> ResponseRepresentable {
-    return ""
+    guard let content = request.body.bytes, !content.isEmpty else {
+      return Response.missingParameters
+    }
+    return try imageViewMapper.map(
+      try uploadImage.execute(with: content)
+    ).makeJSON()
   }
 }
